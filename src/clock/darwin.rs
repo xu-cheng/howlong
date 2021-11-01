b6fa26b53c31ca4c43f8ecb05b19879347713bf7
Cheng XU <git@xuc.me>
2021-10-31 22:06:18 -0700

    chore: fix clippy lint

--- src/clock/darwin.rs
+++ src/clock/darwin.rs
@@ -13,7 +13,6 @@ mod mach {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
     #![allow(non_upper_case_globals)]
-    #![allow(safe_packed_borrows)]
     include!(concat!(env!("OUT_DIR"), "/darwin_bindings.rs"));
 }
 
